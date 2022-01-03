37dd6b5e8d8b035ee9270fcdcd99a8fdaaa22e07
Outlaw0488 <92222146+Outlaw0488@users.noreply.github.com>
2022-01-03 21:22:14 +0100

    Update volby_2017.py

--- volby_2017.py
+++ volby_2017.py
@@ -19,7 +19,10 @@ def vytvor_odkazy_obci(link: str):
     linky_obci = dict()
     html_okres = requests.get(link)
     soup_okres = bs4.BeautifulSoup(html_okres.text, "html.parser")
-    nazev_okresu = soup_okres.find(string=re.compile("Okres: ")).strip("\n").split(": ")[1]
+    try:
+      nazev_okresu = soup_okres.find(string=re.compile("Okres: ")).strip("\n").split(": ")[1]
+    except:
+      nazev_okresu = soup_okres.find(string=re.compile("Kraj: ")).strip("\n").split(": ")[1]
     obec_odkaz = soup_okres.find_all("td", {"class" : "cislo"})
     linky_obci.update({a_elem.a.get_text():"https://volby.cz/pls/ps2017nss/"+a_elem.a["href"] for a_elem in obec_odkaz})
     print(f"Prohlížím jednotlivé obce pro okres - {nazev_okresu}")
@@ -27,7 +30,13 @@ def vytvor_odkazy_obci(link: str):
 
 def data_obci(slovnik_obci: dict):
     """ prochází stránku s daty o volbach dane obce a rovnou připravuje listy a slovníky pro zapisování dat do CSV"""
-
+    strany = ['Občanská demokratická strana', 'Řád národa - Vlastenecká unie', 'CESTA ODPOVĚDNÉ SPOLEČNOSTI', 'Česká str.sociálně demokrat.','Volte Pr.Blok www.cibulka.net',
+          'Radostné Česko', 'STAROSTOVÉ A NEZÁVISLÍ', 'Komunistická str.Čech a Moravy', 'Strana zelených',
+          'ROZUMNÍ-stop migraci,diktát.EU','Společ.proti výst.v Prok.údolí', 'Strana svobodných občanů', 'Blok proti islam.-Obran.domova',
+          'Občanská demokratická aliance', 'Česká pirátská strana', 'OBČANÉ 2011-SPRAVEDL. PRO LIDI', 'Unie H.A.V.E.L.', 'Referendum o Evropské unii', 'TOP 09', 'ANO 2011',
+          'Dobrá volba 2016', 'SPR-Republ.str.Čsl. M.Sládka', 'Křesť.demokr.unie-Čs.str.lid.',
+          'Česká strana národně sociální', 'REALISTÉ', 'SPORTOVCI', 'Dělnic.str.sociální spravedl.',
+          'Svob.a př.dem.-T.Okamura (SPD)', 'Strana Práv Občanů']
     list_dat = []
     for kod, link in slovnik_obci.items():
         html_obec = requests.get(link)
@@ -41,8 +50,11 @@ def data_obci(slovnik_obci: dict):
         dict_obsahu["vydané obálky"] = soup_obec.find("td", {"class": "cislo", "headers": "sa3"}).text
         dict_obsahu["platné hlasy"] = soup_obec.find("td", {"class": "cislo", "headers": "sa6"}).text
         # najdu jméno strany a pro každou skočím 2 místa dál a získám počet hlasů. asi složitějš, ale aspoň najdu počty přesně ke každé straně
-        for strana in soup_obec.find_all("td", {"class": "overflow_name"}):
-          dict_obsahu[strana.text] = soup_obec.find("td", string=re.compile(strana.text[:20])).next_sibling.next_sibling.text
+        for strana in strany:
+          try:
+            dict_obsahu[strana] = soup_obec.find("td", string=re.compile(strana[:20])).next_sibling.next_sibling.text
+          except: #pokud nenajde danou stranu na webove strance obce, tak dej prazdnou bunku s pocty hlasu
+            dict_obsahu[strana] = " "
         # slovnik s daty kazde obce nahraju postupne do listu, aby to pak zpracovala whiterows funkce
         list_dat.append(dict_obsahu)
     print("Data obcí zapsána")
